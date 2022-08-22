e676a2ceb39bf021c5c10caaa7a4b55559875236
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-08-22 20:34:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+const event = "surprise";
+
+function writeCards(names, anEvent) {
+  let names1 = [];
+  for (let i = 0; i < names.length; i++) {
+    names1.push(`Thank you, ${names[i]}, for the wonderful ${anEvent} gift!`)
+  }
+  return names1;
+}
+
+
+function countDown(j) {
+  while (j >= 0) {
+    console.log(j);
+    j--;
+  }
+}
+
+
